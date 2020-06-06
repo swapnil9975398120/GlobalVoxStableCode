d3c3f1af546a0e5a66cf38ea8420b93f3069166a
unknown <swapnil.ahirrao@mobisoftinfotech.com>
2020-06-06 12:20:05 +0530

    issue resolved

---
+++ src/app/app.module.ts
@@ -0,0 +1,64 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MeetingDetailsComponent } from './meeting-details/meeting-details.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CalendarModule } from 'primeng-lts/calendar';
+import {InputSwitchModule} from 'primeng-lts/inputswitch';
+import { ButtonModule } from 'primeng-lts/components/button/button';
+import { DropdownModule } from 'primeng-lts/components/dropdown/dropdown';
+import { LanguageTranslatorService } from './language-translator.service';
+// import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+// import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { StorageService } from './storage.service';
+import { LocalService } from './local.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MeetingDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    CalendarModule,
+    FormsModule,
+    ReactiveFormsModule,
+    InputSwitchModule,
+    ButtonModule,
+    DropdownModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: LanguageTranslatorService,
+          deps: [HttpClient]
+      }
+  })
+    
+  ],
+  providers: [LanguageTranslatorService,StorageService,LocalService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+  
+    // this language will be used as a fallback when a translation isn't found in the current language
+  //   translate.setDefaultLang('en');
+  //   // the lang to use, if the lang isn't available, it will use the current loader to get them
+  //   translate.use('en');
+//   constructor(private translate: TranslateService) {
+//     translate.setDefaultLang('en');
+// }
+
+  }
+
+  export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http);
+}
---
+++ src/app/meeting-details/meeting-details.component.ts
@@ -0,0 +1,104 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { AppSetting } from '../app-setting';
+import * as moment from 'moment'
+import { LocalService } from '../local.service';
+
+//import { StorageServiceService } from '../storage-service.service';
+// import { AppSettings } from '../../../shared/app.settings';
+
+@Component({
+  selector: 'app-meeting-details',
+  templateUrl: './meeting-details.component.html',
+  styleUrls: ['./meeting-details.component.css']
+})
+export class MeetingDetailsComponent implements OnInit {
+  meetingDetailForm : FormGroup;
+  // minDate: any;
+  // maxDate: any;
+  dateFormat;
+  checked:boolean;
+  labelCancel = "Cancel";
+  labelSave = "Save";
+  minDate = new Date();
+  maxDate = new Date();
+  selectedTimeInMillis = 0;
+  timeZoneOptions: Array<any> = [];
+  timeZone;
+  key:any;
+  retriveData : Array<any> = []
+  constructor(
+    private formBuilder: FormBuilder,
+    private localService: LocalService
+
+    // private storageService: StorageServiceService
+) { }
+
+  ngOnInit() {
+    this.timeZoneOptions = AppSetting.TIMEZONE;
+    this.meetingDetailForm = this.formBuilder.group({
+      fullName: ['', [
+        Validators.required,
+       
+      ]],
+      meetingDate: ['', [
+    
+      ]],
+      startTime: ['', [
+    
+      ]],
+      endTime: ['', [
+    
+      ]],
+      timeZone: ['', [
+      ]],
+      checked: ['', [
+      ]],
+
+    });
+    
+ 
+  }
+
+  getTime(event) {
+     const hours = moment(event).hour();
+      const mins = moment(event).minute();
+    const millisInAMinute = 60000;
+    this.selectedTimeInMillis = (((hours * 60) + mins) * millisInAMinute);
+    
+  }
+
+  onSelectStartDate() {
+    this.maxDate = this.meetingDetailForm.controls.meetingDate.value;
+  }
+
+  onTimeZoneChange(e){
+
+  }
+
+  addForm() {
+    const data = this.meetingDetailForm.value;
+    data.startTime = this.formatDateCell(this.meetingDetailForm.controls.startTime.value.getTime());
+    data.endTime = this.formatDateCell(this.meetingDetailForm.controls.endTime.value.getTime());
+   // data.meetingDate = this.meetingDetailForm.controls.meetingDate.value;
+
+     //Encryption
+    //  this.localService.setJsonValue('user', JSON.stringify(data));
+     
+     //Decryption
+    // const user = this.localService.getJsonValue('user');
+    localStorage.setItem(this.key, JSON.stringify(data));
+    this.retriveData.push(JSON.parse(localStorage.getItem(this.key)));
+    this.meetingDetailForm.reset();
+    this.localService.clearToken();
+
+  }
+
+   formatDateCell(timestamp) {
+     return moment(timestamp).format('hh:mm A');
+  }
+  
+  cancel() {
+     this.meetingDetailForm.reset();
+  }
+}
